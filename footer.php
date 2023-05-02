91df8d62dc40e2606136300f1a030b4d3383445d
sahiljangra18 <132386328+sahiljangra18@users.noreply.github.com>
2023-05-02 22:42:19 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2023. Developed by Sahil Jangra : 21BCS1611.
+<br/>
+Online Voting System.
+<br/>
+2nd Year Project 2023. Chandigarh University
+<br/>
+Contact: <a href="tel:+91 9416353990">+91 9416353990</a> | Email: <a href="mailto:jangrasahil2021@gmail.com">jangrasahil2021@gmail.com</a>
+
+</footer>
---
+++ slider.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/SliderCss.css">
+</head>
+<body>
+
+</body>
+</html>
+<div id="slider">
+	<figure>
+<img src="img/chandigarh1.jpg">
+<img src="img/chandigarh2.jpg">
+<img src="img/chandigarh3.jpg">
+<img src="img/chandigarh4.jpg">
+<img src="img/chandigarh5.jpg">
+    </figure>
+</div>
