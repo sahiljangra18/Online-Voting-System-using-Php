a9db86124b3e0fd6a1763d80bbbe51369128092e
sahiljangra18 <132386328+sahiljangra18@users.noreply.github.com>
2023-05-02 22:58:31 +0530

    Add files via upload

---
+++ register/footer.php
@@ -0,0 +1,12 @@
+
+<div class="footer">
+	<hr/>
+&copy2018. Developed by Sahil Jangra : 21BCS1611.
+<br/>
+Online Voting System.
+<br/>
+Final Year Project 2018. Chandigarh University
+<br/>
+Contact: <a href="tel:+919416353990">+91 9416353990</a> | Email: <a href="jangrasahil2021@gmail.com">jangrasahil2021@gmail.com</a>
+
+</div>
